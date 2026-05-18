ca2fb1f9e7355958bda81acd26a994dd1464ad30
MannantaB <mannanta18@gmail.com>
2026-05-18 21:52:42 +0700

    feat(auth): add user registration handler

--- src/main/java/com/deploy/pertemuan11/controller/AuthController.java
+++ src/main/java/com/deploy/pertemuan11/controller/AuthController.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,5 +20,13 @@ public class AuthController {
         model.addAttribute("request", new RegisterRequest());
 
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute RegisterRequest request) {
+
+        authService.register(request);
+
+        return "redirect:/login";
     }
 }
