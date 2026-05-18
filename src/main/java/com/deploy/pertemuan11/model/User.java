21635f311694bde6719fff0ebd8db2ac5e6524f2
MannantaB <mannanta18@gmail.com>
2026-05-18 10:51:43 +0700

    git commit -m "feat: add one-to-one mapping between User and Profile"

--- src/main/java/com/deploy/pertemuan11/model/User.java
+++ src/main/java/com/deploy/pertemuan11/model/User.java
@@ -1,4 +1,20 @@
 package com.deploy.pertemuan11.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Profile profile;
 }
